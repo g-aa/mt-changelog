bac5367a9720ffc6e0be0705ee9bd59a8a6b7ed1
Andrey Georgievskiy <73390983+g-aa@users.noreply.github.com>
2022-09-05 20:18:49 +0300

    MT-0003: Добавление пакета Logic

---
+++ src/Mt.ChangeLog.Logic/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Mt.Utilities;
+
+namespace Mt.ChangeLog.Logic
+{
+    /// <summary>
+    /// Методы расширения для <see cref="IServiceCollection"/>.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Добавить компоненты логики в коллекцию сервисов.
+        /// </summary>
+        /// <param name="services">Коллекция сервисов.</param>
+        /// <returns>Модифицированная коллекция сервисов.</returns>
+        public static IServiceCollection AddLogic(this IServiceCollection services)
+        {
+            Check.NotNull(services, nameof(services));
+            services.AddMediatR(typeof(ServiceCollectionExtensions).Assembly);
+            return services;
+        }
+    }
+}
