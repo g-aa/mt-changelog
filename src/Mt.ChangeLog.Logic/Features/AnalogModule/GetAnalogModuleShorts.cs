0a31d6f5eb6994947c79918da5163f6f116b7bc7
Andrey Georgievskiy <73390983+g-aa@users.noreply.github.com>
2022-09-05 21:42:54 +0300

    MT-0003: Добавление базовых компонентов в Logic

---
+++ src/Mt.ChangeLog.Logic/Features/AnalogModule/GetAnalogModuleShorts.cs
@@ -0,0 +1,70 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Mt.ChangeLog.Logic.Models;
+using Mt.ChangeLog.TransferObjects.AnalogModule;
+using Mt.Utilities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+using Mt.ChangeLog.Repositories.Abstractions.Interfaces;
+
+namespace Mt.ChangeLog.Logic.Features.AnalogModule
+{
+    /// <summary>
+    /// Запрос на получение перечня кратких моделий данных <see cref="AnalogModuleShortModel"/>.
+    /// </summary>
+    public static class GetAnalogModuleShorts
+    {
+        /// <inheritdoc />
+        public sealed class Command : MtQuery<Unit, IEnumerable<AnalogModuleShortModel>>
+        {
+            /// <summary>
+            /// Инициализация нового экземпляра класса <see cref="Command"/>.
+            /// </summary>
+            /// <param name="unit">Тип void.</param>
+            public Command(Unit unit) : base(unit)
+            {
+            }
+            
+            /// <inheritdoc />
+            public override string ToString()
+            {
+                return $"{base.ToString()} - получение перечня моделей вида {nameof(AnalogModuleShortModel)}.";
+            }
+        }
+
+        /// <inheritdoc />
+        public sealed class Handler : IRequestHandler<Command, IEnumerable<AnalogModuleShortModel>>
+        {
+            /// <summary>
+            /// Журнал логирования.
+            /// </summary>
+            private readonly ILogger<Handler> logger;
+
+            /// <summary>
+            /// Репозиторий для доступа к данным.
+            /// </summary>
+            private readonly IAnalogModuleRepository repository;
+
+            /// <summary>
+            /// Инициализация нового экземпляра класса <see cref="Handler"/>.
+            /// </summary>
+            /// <param name="logger">Журнал логирования.</param>
+            /// <param name="repository">Репозиторий для доступа к данным.</param>
+            public Handler(ILogger<Handler> logger, IAnalogModuleRepository repository)
+            {
+                this.logger = Check.NotNull(logger, nameof(logger));
+                this.repository = Check.NotNull(repository, nameof(repository));
+            }
+
+            /// <inheritdoc />
+            public async Task<IEnumerable<AnalogModuleShortModel>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                Check.NotNull(request, nameof(request));
+                this.logger.LogInformation(request.ToString());
+                var result = await this.repository.GetShortEntitiesAsync();
+                return result;
+            }
+        }
+    }
+}
---
+++ src/Mt.ChangeLog.Logic/Pipelines/ValidationRequestPreProcessor.cs
@@ -0,0 +1,47 @@
+﻿using FluentValidation;
+using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+using Mt.Utilities;
+using System;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Mt.ChangeLog.Logic.Pipelines
+{
+    /// <summary>
+	/// Препроцессор валидации.
+	/// </summary>
+	/// <typeparam name="TRequest">Тип запроса.</typeparam>
+    public sealed class ValidationRequestPreProcessor<TRequest> : IRequestPreProcessor<TRequest>
+    {
+        /// <summary>
+		/// Журнал логирования.
+		/// </summary>
+		private readonly ILogger<ValidationRequestPreProcessor<TRequest>> logger;
+
+        /// <summary>
+        /// Валидатор данных запроса.
+        /// </summary>
+        private readonly IValidator<TRequest> validator;
+
+        /// <summary>
+		/// Инициализация экземпляра класса <see cref="ValidationRequestPreProcessor{TRequest}"/>.
+		/// </summary>
+		/// <param name="logger">Журнал логирования.</param>
+		/// <param name="validator">Валидатор данных запроса.</param>
+		/// <exception cref="ArgumentNullException">Срабатывает если входные параметры равны null.</exception>
+		public ValidationRequestPreProcessor(ILogger<ValidationRequestPreProcessor<TRequest>> logger, IValidator<TRequest> validator)
+        {
+            this.logger = Check.NotNull(logger, nameof(logger));
+            this.validator = Check.NotNull(validator, nameof(validator));
+        }
+
+        /// <inheritdoc />
+		public Task Process(TRequest request, CancellationToken cancellationToken)
+        {
+            Check.NotNull(request, nameof(request));
+            logger.LogInformation($"{request} - валидация параметров.");
+            return Task.CompletedTask;
+        }
+    }
+}
