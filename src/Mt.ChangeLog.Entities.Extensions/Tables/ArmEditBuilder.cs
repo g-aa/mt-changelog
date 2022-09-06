ab9af24fa6297928b7deba7729759e40fdb9f883
Andrey Georgievskiy <73390983+g-aa@users.noreply.github.com>
2022-09-06 20:41:49 +0300

    MT-0003: Добавление расширений для ArmEdit

---
+++ src/Mt.ChangeLog.Entities.Extensions/Tables/ArmEditBuilder.cs
@@ -0,0 +1,69 @@
+﻿using Mt.ChangeLog.Entities.Tables;
+using Mt.ChangeLog.TransferObjects.ArmEdit;
+using System;
+
+namespace Mt.ChangeLog.Entities.Extensions.Tables
+{
+    /// <summary>
+    /// Строитель <see cref="ArmEdit"/>.
+    /// </summary>
+    public class ArmEditBuilder
+    {
+        private readonly ArmEdit entity;
+
+        private string divg;
+        private string version;
+        private DateTime? date;
+        private string description;
+
+        /// <summary>
+        /// Инициализация экземпляра класса <see cref="ArmEditBuilder"/>.
+        /// </summary>
+        /// <param name="entity">Сущность.</param>
+        public ArmEditBuilder(ArmEdit entity) 
+        {
+            this.entity = entity;
+        }
+
+        /// <summary>
+        /// Добавить атрибуты.
+        /// </summary>
+        /// <param name="model"Модель.</param>
+        /// <returns>Строитель.</returns>
+        public ArmEditBuilder SetAttributes(ArmEditModel model)
+        {
+            this.divg = model?.DIVG;
+            this.version = model?.Version;
+            this.date = model?.Date;
+            this.description = model?.Description;
+            return this;
+        }
+
+        /// <summary>
+        /// Построить сущность.
+        /// </summary>
+        /// <returns>Сущность.</returns>
+        /// <exception cref="ArgumentException">Ошибка в логике обработки связей.</exception>
+        public ArmEdit Build()
+        {
+            // атрибуты:
+            // this.entity.Id - не обновляется!
+            this.entity.DIVG = this.divg;
+            this.entity.Version = this.version;
+            this.entity.Date = date != null ? date.Value : DateTime.Now;
+            this.entity.Description = description;
+            // реляционные связи:
+            // this.entity.ProjectRevisions - не обновляется!
+            return this.entity;
+        }
+
+        /// <summary>
+        /// Получить строитель.
+        /// </summary>
+        /// <returns>Строитель.</returns>
+        public static ArmEditBuilder GetBuilder()
+        {
+            return new ArmEditBuilder(new ArmEdit());
+        }
+    }
+}
---
+++ src/Mt.ChangeLog.Entities.Extensions/Tables/ArmEditExtensions.cs
@@ -0,0 +1,57 @@
+﻿using Mt.ChangeLog.Entities.Tables;
+using Mt.ChangeLog.TransferObjects.ArmEdit;
+using Mt.Utilities;
+
+namespace Mt.ChangeLog.Entities.Extensions.Tables
+{
+    /// <summary>
+    /// Методы расширения для класса <see cref="ArmEdit"/>.
+    /// </summary>
+    public static class ArmEditExtensions
+    {
+        /// <summary>
+        /// Преобразовать сущность <see cref="ArmEdit"/> в модель <see cref="ArmEditShortModel"/>.
+        /// </summary>
+        /// <param name="entity">Сущность.</param>
+        /// <returns>Модель.</returns>
+        public static ArmEditShortModel ToShortModel(this ArmEdit entity)
+        {
+            Check.NotNull(entity, nameof(entity));
+            var result = new ArmEditShortModel()
+            {
+                Id = entity.Id,
+                Version = entity.Version
+            };
+            return result;
+        }
+
+        /// <summary>
+        /// Преобразовать сущность <see cref="ArmEdit"/> в модель <see cref="ArmEditModel"/>.
+        /// </summary>
+        /// <param name="entity">Сущность.</param>
+        /// <returns>Модель.</returns>
+        public static ArmEditModel ToModel(this ArmEdit entity)
+        {
+            Check.NotNull(entity, nameof(entity));
+            var result = new ArmEditModel()
+            {
+                Id = entity.Id,
+                Date = entity.Date,
+                DIVG = entity.DIVG,
+                Version = entity.Version,
+                Description = entity.Description
+            };
+            return result;
+        }
+
+        /// <summary>
+        /// Получить строитель.
+        /// </summary>
+        /// <param name="entity">Сущность.</param>
+        /// <returns>Строитель.</returns>
+        public static ArmEditBuilder GetBuilder(this ArmEdit entity)
+        {
+            return new ArmEditBuilder(Check.NotNull(entity, nameof(entity)););
+        }
+    }
+}
