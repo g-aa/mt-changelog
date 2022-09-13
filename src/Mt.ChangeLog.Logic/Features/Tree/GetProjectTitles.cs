7f2b6fa5b4736e9ebe9068b4e720b7cf321c9f0e
Andrey Georgievskiy <73390983+g-aa@users.noreply.github.com>
2022-09-13 20:58:34 +0300

    MT-0003: Обновление пакета WebAPI

---
+++ src/Mt.ChangeLog.Logic/Features/Tree/GetProjectTitles.cs
@@ -0,0 +1,75 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Mt.ChangeLog.Context;
+using Mt.ChangeLog.Logic.Models;
+using Mt.Utilities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mt.ChangeLog.Logic.Features.Tree
+{
+    /// <summary>
+    /// Запрос на получение перечня наименование версий проектов.
+    /// </summary>
+    public static class GetProjectTitles
+    {
+        /// <inheritdoc />
+        public sealed class Query : MtQuery<Unit, IEnumerable<string>>
+        {
+            /// <summary>
+            /// Инициализация нового экземпляра класса <see cref="Query"/>.
+            /// </summary>
+            public Query() : base(Unit.Value)
+            {
+            }
+
+            /// <inheritdoc />
+            public override string ToString()
+            {
+                return $"{base.ToString()} - получение перечня наименование версий проектов.";
+            }
+        }
+
+        /// <inheritdoc />
+        public sealed class Handler : IRequestHandler<Query, IEnumerable<string>>
+        {
+            /// <summary>
+            /// Журнал логирования.
+            /// </summary>
+            private readonly ILogger<Handler> logger;
+
+            /// <summary>
+            /// Контекст данных.
+            /// </summary>
+            private readonly ApplicationContext context;
+
+            /// <summary>
+            /// Инициализация нового экземпляра класса <see cref="Handler"/>.
+            /// </summary>
+            /// <param name="logger">Журнал логирования.</param>
+            /// <param name="context">Контекст данных.</param>
+            public Handler(ILogger<Handler> logger, ApplicationContext context)
+            {
+                this.logger = Check.NotNull(logger, nameof(logger));
+                this.context = Check.NotNull(context, nameof(context));
+            }
+
+            /// <inheritdoc />
+            public async Task<IEnumerable<string>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                Check.NotNull(request, nameof(request));
+                this.logger.LogInformation(request.ToString());
+
+                var result = this.context.ProjectVersions.AsNoTracking()
+                    .Select(e => e.Title)
+                    .Distinct()
+                    .OrderBy(e => e);
+
+                return await Task.FromResult(result);
+            }
+        }
+    }
+}
---
+++ src/Mt.ChangeLog.WebAPI/Controllers/V1/HistoryController.cs
@@ -0,0 +1,54 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mt.ChangeLog.TransferObjects.Historical;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mt.ChangeLog.WebAPI.Controllers.V1
+{
+    /// <summary>
+    /// Контроллер для работы с историями изменений.
+    /// </summary>
+    [Route("api/history")]
+    public class HistoryController : ApiControllerBase
+    {
+        /// <summary>
+        /// Инициализация экземпляра класса <see cref="HistoryController"/>.
+        /// </summary>
+        /// <param name="mediator">Медиатор.</param>
+        public HistoryController(IMediator mediator) : base(mediator)
+        {
+        }
+
+        /// <summary>
+        /// Получить историю изменения для версии проета.
+        /// </summary>
+        /// <param name="id">Идентификатор версии проекта.</param>
+        /// <param name="token">Токен отмены.</param>
+        /// <returns>Результат действия.</returns>
+        [HttpGet]
+        [Route("version/{id:guid}")]
+        [SwaggerResponse(StatusCodes.Status200OK, "История изменения версии проекта.", typeof(ProjectVersionHistoryModel))]
+        public async Task<IActionResult> GetVersionHistoryModel([FromRoute] Guid id, CancellationToken token = default)
+        {
+            return this.Ok();
+        }
+
+        /// <summary>
+        /// Получить историю изменения для редакции проета.
+        /// </summary>
+        /// <param name="id">Идентификатор редакции проекта.</param>
+        /// <param name="token">Токен отмены.</param>
+        /// <returns>Результат действия.</returns>
+        [HttpGet]
+        [Route("revision/{id:guid}")]
+        [SwaggerResponse(StatusCodes.Status200OK, "История изменения редакции проекта.", typeof(ProjectRevisionHistoryModel))]
+        public async Task<IActionResult> GetRevisionHistoryModel([FromRoute] Guid id, CancellationToken token = default)
+        {
+            return this.Ok();
+        }
+    }
+}
